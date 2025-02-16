a44c8e4024e2174cdddb6ea17ad06cd5e2667cd2
Ravou <oliviaravou@hotmail.fr>
2025-02-16 14:25:58 +0100

    lol

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,8 +5,8 @@
  * Return : Always 0 (success)
  */
 
-init main (void)
+int main (void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\"");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
