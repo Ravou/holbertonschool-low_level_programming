31891b80d6002edc5f40f9ae8f0e9de5a618451c
Ravou <oliviaravou@hotmail.fr>
2025-02-16 14:14:15 +0100

    lol

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+/**
+ * main - print "with proper grammar, but the outcome is a piece of art,"
+ * Return : Always 0 (success)
+ */
+
+init main (avoid)
+{
+	printif ("with proper grammar, but the outcome is a piece of art,");
+	return (0); 
