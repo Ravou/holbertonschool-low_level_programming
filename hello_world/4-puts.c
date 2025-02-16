0763ae8b45cd53561e11487814b0e03aab171e5a
Ravou <oliviaravou@hotmail.fr>
2025-02-16 12:43:56 +0100

    lol

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Programming is like building a multilingual puzzle
- * Return:0 
+ * main - affiche rogramming is like building a multilingual puzzle
+ * Return:Always 0 
  */
 int main(void) 
 {
