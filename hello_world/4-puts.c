79a0c422cecc71ce6387288a5640e9b9cb4e37da
Ravou <oliviaravou@hotmail.fr>
2025-02-16 13:22:18 +0100

    lol

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - print "Programming is like building a multilingual puzzle, followed by a newline."
- * Return: Always 0 (succès) 
+ * Return: Always 0 (success)
  */
+
 int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
-	return 0;       
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);       
 }
