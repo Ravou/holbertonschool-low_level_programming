9d52ad15336cd69cc66d8a88038669b43bc8e95f
Ravou <oliviaravou@hotmail.fr>
2025-02-18 11:14:08 +0100

    lol

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,6 +3,10 @@
  * main - entry point
  * Return: Always (0) (Success)
  */
-int main (avoid)
-	putchar
-
+int main() {
+    char c;
+    for (c = 'A'; c <= 'Z'; c++) {
+        putchar(c);
+    }
+    putchar('\n'); // Nouvelle ligne après l'affichage des lettres
+    return 0;
