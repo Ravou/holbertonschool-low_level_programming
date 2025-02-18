eaf0f0e8193da6c1be4bfa179bc6c077076be1d8
Ravou <oliviaravou@hotmail.fr>
2025-02-18 21:38:23 +0100

    premier essaie

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always (0) (Success)
+ *
+ */
+int main(void)
+{
+	int c;
+
+	for(c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
