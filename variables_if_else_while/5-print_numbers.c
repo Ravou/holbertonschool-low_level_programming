b155ca1691d11378481d7c35915cbf49e45edd90
Ravou <oliviaravou@hotmail.fr>
2025-02-18 19:30:26 +0100

    premier essaie

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always (0) (success)
+ *
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d\n", c);
+	}
+	return (0);
+}
