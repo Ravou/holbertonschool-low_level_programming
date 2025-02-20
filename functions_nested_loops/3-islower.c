55c28b44337f46ec804ca1116e5cd886b1344694
Ravou <oliviaravou@hotmail.fr>
2025-02-20 21:14:48 +0100

    essaie une

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,48 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'A'; c <= 'z'; c++)
+	{
+		if (islower(c))
+		{
+			_putchar(c);
+			_putchar('l');
+			_putchar('o');
+			_putchar('w');
+			_putchar('e');
+			_putchar('r');
+			_putchar('c');
+			_putchar('a');
+			_putchar('s');
+			_putchar('e');
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(c);
+			_putchar('n');
+			_putchar('o');
+			_putchar('t');
+			_putchar(' ');
+			_putchar('l');
+			_putchar('o');
+			_putchar('w');
+			_putchar('e');
+			_putchar('r');
+			_putchar('c');
+			_putchar('a');
+			_putchar('s');
+			_putchar('e');
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
