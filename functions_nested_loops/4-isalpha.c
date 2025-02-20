d3982fec15608eff75db9a2c6502661a6deb3a4c
Ravou <oliviaravou@hotmail.fr>
2025-02-20 23:41:12 +0100

    description _isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * isalpha - permet de vérifier les caractères alphabétique seulement
- * @c: il est le local de standard c
+ * _isalpha - permet de vérifier les caractères alphabétique seulement
+ * @c: il est le caractère a tester
  * Return: Si c'est une lettre en Majiscule ou miniscule (1) sinon (0)
- *
  */
 int _isalpha(int c)
 {
