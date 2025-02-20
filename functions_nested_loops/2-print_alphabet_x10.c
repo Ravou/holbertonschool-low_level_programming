2044f0d21c181a6cd2d99affe816ba9759476757
Ravou <oliviaravou@hotmail.fr>
2025-02-20 16:20:50 +0100

    essaie avec *

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c * 10)
+	}
+	_putchar('\n')
+}
