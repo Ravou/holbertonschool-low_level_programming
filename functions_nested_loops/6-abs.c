6891c6978d1dd47efd24c1b551130bc4ed49a4bd
Ravou <oliviaravou@hotmail.fr>
2025-02-21 15:28:07 +0100

    erreur

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - calcul la valeur absolue d'un entier
+ * @j: permet de donner d'une valeur absolue d'un nombre
+ * Return: Always (0) (Success)
+ *
+ */
+int _abs(int)
+{
+	int j;
+
+	if (j >= 0)
+	{
+		return (1);
+	}
+	else(j <= 0);
+	{
+		return (0);
+	}
+	return (0);
+}
