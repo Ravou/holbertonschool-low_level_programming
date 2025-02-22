3fb1731417726a05ca31d7e99a6dd536874c414a
Ravou <oliviaravou@hotmail.fr>
2025-02-22 18:31:28 +0100

    lol

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * time_table - afficher les tables de 0 à 9
+ * 
+ *
+ */
+void time_table(void)
+{
+	int n, m;
+	int multi;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 9; m++)
+			multi = (n * m) + '0';
+		_putchar(multi);
+		_putchar(',');
+		_putchar('\n');
+	}
+}
