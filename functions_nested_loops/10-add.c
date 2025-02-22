d8747691efeb3997d560e77cfb2295d36c79bca6
Ravou <oliviaravou@hotmail.fr>
2025-02-22 20:25:22 +0100

    lol

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -8,6 +8,7 @@
 int add(int n, int m)
 {
 	int result = n + m;
+	int digit;
 
 	if (result < 0)
 	{
@@ -19,7 +20,23 @@ int add(int n, int m)
 	{
 		_putchar('0');
 	}
-	_putchar((result % 10) + '0');
+	else
+	{
+		int divisor = 1000000000;
+		int leading_zero = 1;
 
+		while (divisor > 0)
+		{
+			digit = result / divisor;
+			result %= divisor;
+			divisor /= 10;
+
+			if (digit != 0 || !leading_zero)
+			{
+				_putchar( digit + '0');
+				leading_zero = 0;
+			}
+		}
+	}
 	return (0);
 }
