e941db6d5eb2fa59c78b082606333320f613953f
Ravou <oliviaravou@hotmail.fr>
2025-02-22 20:57:21 +0100

    lol

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 /**
  * print_last_digit - afficher le dernier chiffre du dernier nombre
  * @n : permet de trouver le Last digit
- * Return: (Last_digit) (Success)
+ * Return: (last_digit) (Success)
  *
  */
 int print_last_digit(int n)
@@ -12,10 +12,7 @@ int print_last_digit(int n)
 	last_digit = n % 10;
 
 	if (last_digit < 0)
-	{
 		last_digit = -last_digit;
-		_putchar(last_digit + '0');
-		return (last_digit);
-	}
-	return (0);
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
