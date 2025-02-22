b6598d1e922ba6ae3da6fc8cec567cced099f85d
Ravou <oliviaravou@hotmail.fr>
2025-02-22 18:49:42 +0100

    essaie 1

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,14 +7,32 @@
 void times_table(void)
 {
 	int n, m;
-	int multi;
+	int multi1, multi2, multi3, multi4, multi5, multi6, multi7, multi8, multi9, multi10;
 	
 	for (n = 0; n <= 9; n++)
 	{
 		for (m = 0; m <= 9; m++)
 		{
-			multi = (n * m) + '0';
-			_putchar(multi);
+			multi1 = (n * m) + '0';
+			multi2 = (n * m) + '0';
+			multi3 = (n * m) + '0';
+			multi4 = (n * m) + '0';
+			multi5 = (n * m) + '0';
+			multi6 = (n * m) + '0';
+			multi7 = (n * m) + '0';
+			multi8 = (n * m) + '0';
+			multi9 = (n * m) + '0';
+			multi10 = (n * m) + '0';
+			_putchar(multi1);
+			_putchar(multi2);
+			_putchar(multi3);
+			_putchar(multi4);
+			_putchar(multi5);
+			_putchar(multi6);
+			_putchar(multi7);
+			_putchar(multi8);
+			_putchar(multi9);
+			_putchar(multi10);
 			_putchar(',');
 			_putchar('\n');
 		}
