4324851284a6af7483be6e4eea61df39887273f8
Ravou <oliviaravou@hotmail.fr>
2025-02-22 20:44:13 +0100

    lol

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -4,27 +4,9 @@
  * @n: le premier entier à additionner
  * @m: le second entier à additionner
  *
- * Return: Toujours 0 (succès)
+ * Return: la somme de n et m
  */
 int add(int n, int m)
 {
-	int result = n + m;
-	int tens, ones;
-	
-	if (result < 0)
-	{
-		_putchar('-');
-		result = -result;
-	}
-
-	tens = result / 10;
-	ones = result % 10;
-
-	
-	if (tens != 10)
-	{
-		_putchar(tens + '0');
-	}
-	_putchar(ones+ '0');
-	return (0);
+	return (n + m);
 }
