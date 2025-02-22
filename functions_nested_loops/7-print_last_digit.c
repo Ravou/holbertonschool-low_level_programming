83732785ae392e084a8e55d514c918ea929a1aba
Ravou <oliviaravou@hotmail.fr>
2025-02-22 20:53:56 +0100

    lol

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,15 @@
  */
 int print_last_digit(int n)
 {
-	int Last_digit = n % 10;
+	int last_digit;
+       	
+	last_digit = n % 10;
 
-	_putchar(Last_digit + '0');
-	return (Last_digit);
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	return (0);
 }
