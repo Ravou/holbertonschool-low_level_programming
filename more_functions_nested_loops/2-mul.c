e84dd8fcb0cd4087219d2effed69ec912d166133
Ravou <oliviaravou@hotmail.fr>
2025-02-24 21:22:04 +0100

    essaie 4

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * mul - fonction qui permet de multiplié 
+ * mul- fonction qui permet de multiplié
  * @a: Premier nombre
  * @b: Deuxième nombre
  * Return: (a * b)
- *
  */
 int mul(int a, int b)
 {
