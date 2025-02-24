dd46d3b8e3e96510742c893f24f4c1c3964759a4
Ravou <oliviaravou@hotmail.fr>
2025-02-24 21:13:48 +0100

    essaie 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - fonction qui permet de multiplié 
+ * @a: Premier nombre
+ * @b: Deuxième nombre
+ * Return: Always (0) (Success)
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
