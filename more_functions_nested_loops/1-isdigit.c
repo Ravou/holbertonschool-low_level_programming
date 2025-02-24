40c64992ac2948f72cb2b1b2dcc01a8f53fd788a
Ravou <oliviaravou@hotmail.fr>
2025-02-24 19:42:20 +0100

    premier essaie

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - permet de vérifier le digit
+ * @c: est un variable
+ * Return: (1) si c'est un digit, (0) sinon
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
