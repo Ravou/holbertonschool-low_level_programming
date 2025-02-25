f6e89465924b06019f02806092dfd4c9080d33d2
Ravou <oliviaravou@hotmail.fr>
2025-02-25 12:56:01 +0100

    essaie 1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - affiche 10 fois les numéros de 1 à 14
+ *
+ */
+void more_numbers(void)
+{
+	int n;
+	int c;
+
+	for (n = 0 ; n <= 10 ; n++)
+	{
+		for (c = 0 ;  c <= 14 ; c++)
+		{
+			_putchar('0' + (c /10));
+			_putchar('0'+ (c % 10));
+		}
+		_putchar('\n');
+	}
+}
