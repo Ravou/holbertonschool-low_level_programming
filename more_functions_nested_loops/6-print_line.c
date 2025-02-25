c3c59e32a53c26efe9584257c850a3c1fc289181
Ravou <oliviaravou@hotmail.fr>
2025-02-25 14:24:57 +0100

    essaie 1

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - affiche le nombre de caractère
+ *
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar('_');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
