0551593320432189f109e375cfcc9f83f712d59d
Ravou <oliviaravou@hotmail.fr>
2025-02-25 15:25:06 +0100

    essaie 1

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - permet d'afficher le diagonal
+ * @n: affiche le nombre de fois qu'il faut de diagonal
+ *
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('\\');
+			_putchar(' ');
+			_putchar('\n');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
