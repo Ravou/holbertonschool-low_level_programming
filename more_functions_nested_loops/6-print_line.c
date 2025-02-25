2bb215f0e251b722c541f1e394c4cfde3d28014c
Ravou <oliviaravou@hotmail.fr>
2025-02-25 14:42:44 +0100

    essaie 2

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
  * print_line - affiche le nombre de caractère
- *
+ * @n: le nombre de fois de caractère
  */
 void print_line(int n)
 {
+	int i = 0;
+
 	if (n > 0)
 	{
-		_putchar('_');
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
 	}
 	else
 	{
