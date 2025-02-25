aa51426601f93c35419e7d7f7e479663d55a502a
Ravou <oliviaravou@hotmail.fr>
2025-02-25 13:05:41 +0100

    essaie 4

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,10 @@ void more_numbers(void)
 		for (c = 0;  c <= 14; c++)
 		{
 			if (c > 9)
-				_putchar('0' + (c /10));
-			_putchar('0'+ (c % 10));
+			{
+				_putchar('0' + (c / 10));
+			_putchar('0' + (c % 10));
+			}
 		}
 		_putchar('\n');
 	}
