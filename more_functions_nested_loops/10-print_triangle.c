3521cd8ac9f5a97c701ba8acb8546a60340de787
Ravou <oliviaravou@hotmail.fr>
2025-02-26 19:57:11 +0100

    essaie 6

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,22 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
-			for ( j = 0; j < i; j++)
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
 	{
