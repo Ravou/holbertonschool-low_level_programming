30955305320beba5ce6695edda4d7893bc0b457b
Ravou <oliviaravou@hotmail.fr>
2025-02-27 22:16:09 +0100

    essaie avec le bon betty

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * -strlen.c - qui permet de calculer la longueur d'une chaine de caractère.
+ * _strlen - qui permet de calculer la longueur d'une chaine de caractère.
  * @s: permet d'afficher le retour de la longueur de chaine
  * Return: la longueur de chaine
  *
  */
 int _strlen(char *s)
 {
-	int length = 0; 
+	int length = 0;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-	return length;
+	return (length);
 }
