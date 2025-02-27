3cfd562ace4108d0f11d7f160a487551de64085d
Ravou <oliviaravou@hotmail.fr>
2025-02-27 22:22:12 +0100

    essaie 2

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - entry
+ * @str: ptr str
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
