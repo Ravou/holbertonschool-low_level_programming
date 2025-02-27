c92b84437ac3a05aaa0284f76574531a3807e0e5
Ravou <oliviaravou@hotmail.fr>
2025-02-27 18:31:52 +0100

    essaie 2

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98 - met la valeur pointée par n à 98 
- * @n : pointeur vers l'entier à modifier 
+ * reset_to_98 - met la valeur pointée par n à 98
+ *@n: pointeur vers l'entier à modifier
  *
  */
 void reset_to_98(int *n)
