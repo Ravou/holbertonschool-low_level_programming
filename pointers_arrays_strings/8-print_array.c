3f3d11e3ccaab918ef6c1869a3097180a615abe0
Ravou <oliviaravou@hotmail.fr>
2025-03-01 14:36:57 +0100

    essaie 1

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Affiche n éléments d'un tableau d'entiers
+ * @a: Tableau d'entiers à afficher
+ * @n: Nombre d'éléments à afficher
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+		{
+			printf(",");
+		}
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
