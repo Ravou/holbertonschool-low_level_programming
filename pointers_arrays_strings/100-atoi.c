214d95a64b318e1faf9a11cbaf5a28495c879746
Ravou <oliviaravou@hotmail.fr>
2025-03-01 16:46:13 +0100

    essaie 3

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,28 +7,32 @@
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, result = 0;
-    
-    while (s[i] != '\0')
-    {
-	    if (s[i] == '-')
-	    {
-		    sign = -sign;
-	    }
-	    if (s[i] == '+')
-	    {
-		    sign = 1;
-	    }
-	    if (s[i] >= '0' && s[i] <= '9')
-	    {
-		    break;
-	    }
-	    i++;
-    }
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-	    result = result * 10 + (s[i] - '0');
-	    i++;
-    }
-    return result * sign;
+	int i = 0, sign = 1, result = 0;
+	
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9') && s[i] != '+' && s[i] != '-')
+	{
+		i++;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
+		else if (s[i] == '+')
+		{
+			sign = 1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			break;
+		}
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return result * sign;
 }
