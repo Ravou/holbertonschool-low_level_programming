be1c76c70e889814755d06e542c9e18e47d65e10
Ravou <oliviaravou@hotmail.fr>
2025-03-03 11:21:30 +0100

    essaie 1

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - Remplir la mémoire avec un octet constant
+ * @s: La zone mémoire à remplir
+ * @b: L'octet constant pour remplir la mémoire
+ * @n: Le nombre d'octets à remplir
+ * 
+ * Retourne: Un pointeur vers la zone mémoire s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++) 
+	{
+		s[i] = b;
+	}
+	return s;
+}
