211ac5a1962c1e49654b75f805b3d40b7b7cfc6d
Ravou <oliviaravou@hotmail.fr>
2025-03-05 15:28:42 +0100

    essaie 2

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,26 +1,32 @@
 #include "main.h"
+#include <stddef.h>
 /*
  *_strstr - la fonction qui permet de localiser une sous chaine
  *@needle : est la sous chaine
  *@haystack: est la chaine initiale
  *Return :
- *-retour au pointeur vers le début du sous chaine (&needle[i])
- *-retour a (NULL) si je ne trouve pas
+ *-return au pointeur vers le début du sous chaine (&haystack[h])
+ *-return a (NULL) si je ne trouve pas
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, int h, int n;
+	int i;
+	int h = 0;
 
 	while (haystack[h] != '\0')
 	{
 		for (i = 0; needle[i] != '\0'; i++)
 		{
-			if (h > needle[i] && i < haystack[h])
+			if (haystack[h + i] != needle[i])
 			{
-				n++;
-				return (&needle[i]);
+				break;
+			}
+			if (needle[i] == '\0')
+			{
+				return (&haystack[h]);
 			}
 		}
+		h++;
 	}
 	return (NULL);
 }
