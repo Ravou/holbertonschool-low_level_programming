11e3cb11260f3c140e5a01f1cc16a6615282294b
Ravou <oliviaravou@hotmail.fr>
2025-03-06 12:26:12 +0100

    essaie 4

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -4,14 +4,14 @@
  * @s: La zone mémoire à remplir
  * @b: L'octet constant pour remplir la mémoire
  * @n: Le nombre d'octets à remplir
- * 
- * Retourne: Un pointeur vers la zone mémoire s
+ *
+ * Return: Un pointeur vers la zone mémoire s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
-	for (i = 0; i < n; i++) 
+
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
