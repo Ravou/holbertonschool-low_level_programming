70d230e278390c0aceaf54e055f54d83a00b2b3f
Ravou <oliviaravou@hotmail.fr>
2025-03-06 13:26:17 +0100

    essaie 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * *_strchr - la fonction sui permet de localiser un caractère dnass une chaine de caractère
+ * *_strchr - la fonction qui permet de localiser un caractère
  * @c : on doit chercher le caractère stocké dans la variable c
  * @s : est la chaine de caractère
  *
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0; 
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
