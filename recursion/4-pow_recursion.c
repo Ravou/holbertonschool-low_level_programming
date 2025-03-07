ca803b0545b3bf05412b4ecae30034a558ef7a22
Ravou <oliviaravou@hotmail.fr>
2025-03-07 16:14:04 +0100

    essaie 1

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - Calcule la puissance de x élevé à y
+ * @x: La base
+ * @y: L'exposant
+ *
+ * Return: x^y, ou -1 si y est inférieur à 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
