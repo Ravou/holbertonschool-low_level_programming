6211e6119f28012fb04aa522986244a6fe9b4d00
Ravou <oliviaravou@hotmail.fr>
2025-03-07 16:03:01 +0100

    essaie 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stddef.h>
+#define NULL ((void *)0)
 /**
  * _strchr - Locates a character in a string
  * @s: The string to search
  * @c: The character to find
  *
- * Return: Pointer to the first occurrence of c in s, or NULL if not found
+ * Return: Pointer to the first occurrence of (c) in (s), or (NULL) if not found
  */
 char *_strchr(char *s, char c)
 {
