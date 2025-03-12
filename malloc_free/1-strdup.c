e553c740226b2df901da0abc21d2ea938d23e374
Ravou <oliviaravou@hotmail.fr>
2025-03-12 22:03:36 +0100

    essaie 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/*
+ * _strdup - fonction qui retourne a un pointeur le dublon d'une chaine de caractère
+ *@str : la chaine de caractère doublé 
+ *Return : si str est égale à (NULL) et si la mémoire est insuffisant
+ *
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	dup = (char *)malloc((i + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	
+	for (i = 0; str[i] != '\0'; i++)
+		dup[i] = str[i];
+
+	dup[i] = '\0';
+	return (dup);
+}
