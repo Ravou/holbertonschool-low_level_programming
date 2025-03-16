3b1f05380d16b21180ce9a0ea515dc0255c64872
Ravou <oliviaravou@hotmail.fr>
2025-03-16 14:18:34 +0100

    essaie 2

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,16 +4,13 @@
  * malloc_checked - Alloue de la mémoire dynamiquement avec malloc.
  *@b: Taille en octet à allouer
  *
- * Return: Pointeur vers la mémoire allouée. Si l'allocation échoué, le programme termine avec le statut 98.
+ * Return: Pointeur vers la mémoire allouée.
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-
-	ptr = malloc(b);
+	void *ptr = malloc(b);
 	if (ptr == NULL)
-	{
 		exit(98);
-	}
+
 	return (ptr);
 }
