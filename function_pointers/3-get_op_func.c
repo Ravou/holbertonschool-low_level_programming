5a9e0e03411b5674a8a642c480298f39f68ced51
Ravou <oliviaravou@hotmail.fr>
2025-03-21 15:12:54 +0100

    essaie 1

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - Matches operator to function
+ * @s: Operator string (e.g., "+")
+ * Return: Pointer to corresponding function, or NULL if no match
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] =
+
+	{
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if
+			(strcmp(s, ops[i].op) == 0)
+				return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
