bc951f525d85fa327fc79838ce58b6d589a8d6b2
Ravou <oliviaravou@hotmail.fr>
2025-04-01 16:07:18 +0200

    fonction ajout de node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - fonction qui ajoute des node au début de la liste
+ * @head: pointeurs 
+ * @str: qui duplique la liste
+ * Return: adresse des nouvelle éléments ou NULL en cas d'échec
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = len;
+	new_node->next = *head;
+
+	*head = new_node;
+		return (new_node);
+}
