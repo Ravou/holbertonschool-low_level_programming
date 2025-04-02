820cfc37f376aa4d120d4ef6aca657312590708a
Ravou <oliviaravou@hotmail.fr>
2025-04-02 19:00:17 +0200

    fonction qui affiche les éléments

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * print_dlistint - fonction affiche les éléments de la liste
- * @h: pointeur qui pointe vers le node suivant et précédente
+ * @h: pointeur qui pointe vers le premier élément
  * Return: le nombre de nodes
  */
 size_t print_dlistint(const dlistint_t *h)
@@ -11,8 +11,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
+		printf ("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
 	return (count);
-}	
+}
