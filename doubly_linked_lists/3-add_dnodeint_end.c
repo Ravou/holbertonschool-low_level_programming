fb4d5b5d83254e5d761f5897dc0113d0e298eb9c
Ravou <oliviaravou@hotmail.fr>
2025-04-06 18:03:36 +0200

    function add node at the end

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint_end - function that adds a new node at the end
+ * @head: pointer to the list of the structur
+ * @n: integer of list
+ * return: adresse of new element or NULL if it failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *temp;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_node;
+	new_node->prev = temp;
+
+	return (new_node);
+}
