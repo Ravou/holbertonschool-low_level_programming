bc6111a3d15f73a596555cdaca25df2c3c6d1157
Ravou <oliviaravou@hotmail.fr>
2025-04-06 19:00:58 +0200

    correction of betty

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
  * sum_dlistint - function sum of all the data (n) of the list
- * @head: pointer of the header of the list
- * return: (0) if the list is empty
+ *@head: pointer of the header of the list
+ *Return: (0) if the list is empty or (sum)
  */
 int sum_dlistint(dlistint_t *head)
 {
-        int sum = 0;
-        dlistint_t *temp;
+	int sum = 0;
+	dlistint_t *temp;
 
-        temp = head;
-        while (temp != NULL)
-        {
-                sum += temp->n;
-                temp = temp->next;
-        }
-        return (sum);
+	temp = head;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
 }
