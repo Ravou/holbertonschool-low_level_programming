6462316288284e177b379318204f62674941c0e0
Ravou <oliviaravou@hotmail.fr>
2025-04-06 18:57:35 +0200

    function for sum all data (n)

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - function sum of all the data (n) of the list
+ * @head: pointer of the header of the list
+ * return: (0) if the list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+        int sum = 0;
+        dlistint_t *temp;
+
+        temp = head;
+        while (temp != NULL)
+        {
+                sum += temp->n;
+                temp = temp->next;
+        }
+        return (sum);
+}
